60ba140d773fb9103f1d79b19b7002fcbb815eb1
xudahui <tuoluoleaf@gmail.com>
2022-06-28 09:38:35 +0800

    懒汉式单例-线程不安全

---
+++ gupaoedu-vip-pattern/gupaoedu-vip-pattern-singleton/src/main/java/com/gupaoedu/vip/pattern/singleton/lazy/UnSafeLazySingleton.java
@@ -0,0 +1,21 @@
+package com.gupaoedu.vip.pattern.singleton.lazy;
+
+/**
+ * 懒汉式单例-线程不安全
+ * @author xutuo
+ */
+public class UnSafeLazySingleton {
+    private UnSafeLazySingleton() {
+    }
+
+    //静态块，公共内存区域
+    private static UnSafeLazySingleton lazy = null;
+
+    public static UnSafeLazySingleton getInstance(){
+        if(lazy == null){
+            lazy = new UnSafeLazySingleton();
+        }
+        return lazy;
+    }
+
+}
