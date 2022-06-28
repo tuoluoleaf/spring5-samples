0c13bce6314576886f58df2c767f237cf85edf45
xudahui <tuoluoleaf@gmail.com>
2022-06-28 09:45:55 +0800

    单例模式

--- gupaoedu-vip-pattern/gupaoedu-vip-pattern-singleton/src/main/java/com/gupaoedu/vip/pattern/singleton/lazy/LazyDoubleCheckSingleton.java
+++ gupaoedu-vip-pattern/gupaoedu-vip-pattern-singleton/src/main/java/com/gupaoedu/vip/pattern/singleton/lazy/LazyDoubleCheckSingleton.java
@@ -24,6 +24,4 @@ public class LazyDoubleCheckSingleton {
         }
         return lazy;
     }
-
-
 }
--- gupaoedu-vip-pattern/gupaoedu-vip-pattern-singleton/src/main/java/com/gupaoedu/vip/pattern/singleton/lazy/UnSafeLazySingleton.java
+++ gupaoedu-vip-pattern/gupaoedu-vip-pattern-singleton/src/main/java/com/gupaoedu/vip/pattern/singleton/lazy/UnSafeLazySingleton.java
@@ -2,6 +2,7 @@ package com.gupaoedu.vip.pattern.singleton.lazy;
 
 /**
  * 懒汉式单例-线程不安全
+ *
  * @author xutuo
  */
 public class UnSafeLazySingleton {
@@ -11,8 +12,8 @@ public class UnSafeLazySingleton {
     //静态块，公共内存区域
     private static UnSafeLazySingleton lazy = null;
 
-    public static UnSafeLazySingleton getInstance(){
-        if(lazy == null){
+    public static UnSafeLazySingleton getInstance() {
+        if (lazy == null) {
             lazy = new UnSafeLazySingleton();
         }
         return lazy;
